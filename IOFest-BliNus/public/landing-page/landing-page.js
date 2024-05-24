8863af140269b8ae3b0a4f6071e33c56914658bd
kumayoster <140373296+kumayoster@users.noreply.github.com>
2024-05-24 13:21:49 +0700

    Add Lightmode Darkmode
    not yet done

--- IOFest-BliNus/public/landing-page/landing-page.js
+++ IOFest-BliNus/public/landing-page/landing-page.js
@@ -2,53 +2,64 @@ const navbarNav = document.querySelector(".navbar-nav");
 
 // ketika menu di klik
 document.querySelector("#Baju-menu").onclick = () => {
-  navbarNav.classList.toggle("active");
+    navbarNav.classList.toggle("active");
 };
 
 // di klik di luar side bar
 document.addEventListener("click", (e) => {
-  if (
-    !document.querySelector("#Baju-menu").contains(e.target) &&
-    !navbarNav.contains(e.target)
-  ) {
-    navbarNav.classList.remove("active");
-  }
+    if (
+        !document.querySelector("#Baju-menu").contains(e.target) &&
+        !navbarNav.contains(e.target)
+    ) {
+        navbarNav.classList.remove("active");
+    }
 });
 
 var icon = document.getElementById("icon");
-icon.onclick = function(){
-  document.body.classList.toggle("dark-theme")
-  if(document.body.classList.contains(""));
-}
+icon.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains(""));
+};
+
+icon.addEventListener("click", (e) => {
+    const iconEl = e.currentTarget.children[0].children[0];
+    console.log(iconEl.getAttribute("href"));
+    if (iconEl.getAttribute("href") === "#terang") {
+        iconEl.setAttribute("href", "#gelap");
+    } else {
+        iconEl.setAttribute("href", "#terang");
+    }
+});
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides((slideIndex += n));
+    showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides((slideIndex = n));
+    showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
